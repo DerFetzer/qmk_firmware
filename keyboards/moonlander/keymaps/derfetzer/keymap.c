99479e1ce3aa3fbb0a22816fa2cf7c9582976be9
DerFetzer <kontakt@der-fetzer.de>
2022-08-22 15:54:58 +0200

    Move pinkies down

--- keyboards/moonlander/keymaps/derfetzer/keymap.c
+++ keyboards/moonlander/keymaps/derfetzer/keymap.c
@@ -90,19 +90,19 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BONE]     = LAYOUT_moonlander(
-    KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_PSCR,                 KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,
-    KC_TAB,         KC_J,           KC_D,           KC_U,           KC_A,           KC_X,           DE_GRV,                  TG(3),          KC_P,           KC_H,           KC_L,           KC_M,           KC_W,           DE_SS,
-    KC_ESCAPE,      KC_C,           KC_T,           KC_I,           KC_E,           KC_O,           DE_ACUT,                 TG(4),          KC_B,           KC_N,           KC_R,           KC_S,           KC_G,           KC_Q,
-    KC_LSHIFT,      KC_F,           KC_V,           DE_UDIA,        DE_ADIA,        DE_ODIA,                                                 DE_Y,           DE_Z,           KC_COMMA,       KC_DOT,         KC_K,           KC_RSHIFT,
-    OSM(MOD_LCTL),  KC_LGUI,        LALT(KC_LCTRL), KC_LALT,        LT(2,KC_ESCAPE),                KC_PLAY,                 TD(TD_MAC),                     TT(2),          KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,
+    XXXXXXX,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_PSCR,                 KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,
+    KC_ESCAPE,      XXXXXXX,        KC_D,           KC_U,           KC_A,           KC_X,           DE_GRV,                  TG(3),          KC_P,           KC_H,           KC_L,           KC_M,           XXXXXXX,        XXXXXXX,
+    KC_TAB,         KC_J,           KC_T,           KC_I,           KC_E,           KC_O,           DE_ACUT,                 TG(4),          KC_B,           KC_N,           KC_R,           KC_S,           KC_W,           DE_SS,
+    KC_LSHIFT,      KC_C,           KC_V,           DE_UDIA,        DE_ADIA,        DE_ODIA,                                                 DE_Y,           DE_Z,           KC_COMMA,       KC_DOT,         KC_G,           KC_Q,
+    OSM(MOD_LCTL),  KC_F,           KC_LGUI,        KC_LALT,        LT(2,KC_ESCAPE),                KC_PLAY,                 TD(TD_MAC),                     TT(2),          XXXXXXX,        XXXXXXX,        KC_K,           KC_RSHIFT,
                                                                     LT(1,KC_SPACE), KC_DELETE,      KC_CAPS,                 RCTL_T_SPACE,   TD(TD_BS),      LT(1,KC_ENTER)
   ),
   [SYMBOLS]  = LAYOUT_moonlander(
     _______,        KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           XXXXXXX,                 KC_ET_DEV,      KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           XXXXXXX,
-    _______,        DE_DEG,         DE_UNDS,        DE_LBRC,        DE_RBRC,        DE_CIRC,        XXXXXXX,                 XXXXXXX,        DE_EXLM,        DE_LABK,        DE_RABK,        DE_EQL,         DE_AMPR,        DE_AT,
-    _______,        DE_BSLS,        DE_SLSH,        DE_LCBR,        DE_RCBR,        DE_ASTR,        XXXXXXX,                 XXXXXXX,        DE_QUES,        DE_LPRN,        DE_RPRN,        DE_MINS,        DE_COLN,        _______,
-    _______,        DE_HASH,        DE_DLR,         DE_PIPE,        DE_TILD,        DE_GRV,                                                  DE_PLUS,        DE_PERC,        DE_DQUO,        DE_QUOT,        DE_SCLN,        DE_EURO,
-    _______,        _______,        _______,        _______,        _______,                        _______,                 _______,                        _______,        _______,        _______,        _______,        _______,
+    _______,        _______,        DE_UNDS,        DE_LBRC,        DE_RBRC,        DE_CIRC,        XXXXXXX,                 XXXXXXX,        DE_EXLM,        DE_LABK,        DE_RABK,        DE_EQL,         _______,        _______,
+    _______,        DE_DEG,         DE_SLSH,        DE_LCBR,        DE_RCBR,        DE_ASTR,        XXXXXXX,                 XXXXXXX,        DE_QUES,        DE_LPRN,        DE_RPRN,        DE_MINS,        DE_AMPR,        DE_AT,
+    _______,        DE_BSLS,        DE_DLR,         DE_PIPE,        DE_TILD,        DE_GRV,                                                  DE_PLUS,        DE_PERC,        DE_DQUO,        DE_QUOT,        DE_COLN,        _______,
+    _______,        DE_HASH,        _______,        _______,        _______,                        _______,                 _______,                        _______,        _______,        _______,        DE_SCLN,        DE_EURO,
                                                                     _______,        _______,        _______,                 _______,        _______,        _______
   ),
   [CONTROLS] = LAYOUT_moonlander(
